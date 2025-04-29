8b1d621cf59e2818c37031ae998656da7dc19bd2
ALP PİYAN <alppiyan0@gmail.com>
2025-04-29 16:21:29 +0300

    Update App_react and app.py

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
-from flask_cors import CORS # type: ignore
+from flask_cors import CORS
+
 from routes.product import product_bp
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 app.register_blueprint(product_bp, url_prefix='/api/product')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
